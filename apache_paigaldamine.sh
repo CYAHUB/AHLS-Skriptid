2dfd8aceb698f679e1e271294331389a584bcfb1
CYAHUB <Skysassky@gmail.com>
2023-11-01 13:45:12 +0200

    Apache script

---
+++ apache_paigaldamine.sh
@@ -0,0 +1,12 @@
+
+APACHE2=$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c 'ok installed')
+if [ $APACHE2 -eq 0 ]; then
+    echo "Installime apache2"
+    apt install apache2
+    echo "Apache2 on installitud!"
+elif [ $APACHE2 - eq 1 ]; then
+    echo "Apache2 on juba installitud!"
+    echo "Käivitame Apache2 tööle"                                                                                                                  #You fool! CYAHUB 
+    service apache2 start
+    service apache2 status
+fi
