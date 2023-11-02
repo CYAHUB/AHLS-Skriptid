25b71c3347b648239e0068a2a54aa246ac80fe6f
CYAHUB <Skysassky@gmail.com>
2023-11-02 09:43:47 +0200

    Apache ja PHP muudatused

--- apache_paigaldamine.sh
+++ apache_paigaldamine.sh
@@ -1,6 +1,7 @@
 
 APACHE2=$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c 'ok installed')
 if [ $APACHE2 -eq 0 ]; then
+    echo "Masinal ei ole apache2"
     echo "Installime apache2"
     apt install apache2
     echo "Apache2 on installitud!"
